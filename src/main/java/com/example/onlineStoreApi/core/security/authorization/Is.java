867f58196c5eed418558909196699e5291cf9adb
aymansainshy <aymansainshy@gmail.com>
2024-07-22 20:47:23 +0400

    Add Authentication && Authorization with CustomeSecurityExpression

--- src/main/java/com/example/onlineStoreApi/core/security/authorization/Is.java
+++ src/main/java/com/example/onlineStoreApi/core/security/authorization/Is.java
@@ -1,4 +1,4 @@
-package com.example.onlineStoreApi.core.security.authentication;
+package com.example.onlineStoreApi.core.security.authorization;
 
 public class Is {
     public static final String USER = "@customSecurityExpression.isUser(#id)";
