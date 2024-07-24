f9e879c60caf7d13045e3c2e02830659cca284a3
aymansainshy <aymansainshy@gmail.com>
2024-07-24 20:27:59 +0400

    Impelement LoggingFilter

--- src/main/java/com/example/onlineStoreApi/core/filters/FilterConfig.java
+++ src/main/java/com/example/onlineStoreApi/core/filters/FilterConfig.java
@@ -6,19 +6,29 @@ import org.springframework.context.annotation.Configuration;
 public class FilterConfig {
 
 //    @Bean
-//    public FilterRegistrationBean<RequestResponseLoggingFilter> loggingFilter() {
-//        FilterRegistrationBean<RequestResponseLoggingFilter> registrationBean = new FilterRegistrationBean<>();
-//        registrationBean.setFilter(new RequestResponseLoggingFilter());
-//        registrationBean.setOrder(1);  // Set order
+//    public FilterRegistrationBean<LoggingFilter> loggingFilter() {
+//        FilterRegistrationBean<LoggingFilter> registrationBean = new FilterRegistrationBean<>();
+//        registrationBean.setFilter(new LoggingFilter());
+//        registrationBean.addUrlPatterns("/*");
+//        registrationBean.setOrder(1); // Set the order
 //        return registrationBean;
 //    }
 //
 //    @Bean
-//    public FilterRegistrationBean<RequestResponseLoggingFilter2> loggingFilter2() {
-//        FilterRegistrationBean<RequestResponseLoggingFilter2> registrationBean = new FilterRegistrationBean<>();
-//        registrationBean.setFilter(new RequestResponseLoggingFilter2());
-//        registrationBean.setOrder(2);  // Set order
+//    public FilterRegistrationBean<JwtAuthenticationFilter> jwtAuthenticationFilter() {
+//        FilterRegistrationBean<JwtAuthenticationFilter> registrationBean = new FilterRegistrationBean<>();
+//        registrationBean.setFilter(new JwtAuthenticationFilter());
+//        registrationBean.addUrlPatterns("/*");
+//        registrationBean.setOrder(2); // Set the order
 //        return registrationBean;
 //    }
-
+//
+//    @Bean
+//    public FilterRegistrationBean<AnotherLoggingFilter> anotherLoggingFilter() {
+//        FilterRegistrationBean<AnotherLoggingFilter> registrationBean = new FilterRegistrationBean<>();
+//        registrationBean.setFilter(new AnotherLoggingFilter());
+//        registrationBean.addUrlPatterns("/*");
+//        registrationBean.setOrder(3); // Set the order
+//        return registrationBean;
+//    }
 }
